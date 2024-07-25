103ac9b7b99ac833ca34c34633377aba491ad843
tykkmm <tykhanew45@gmail.com>
2024-07-25 21:34:01 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
 # Pyrogram API credentials
-API_ID = int(os.environ.get("API_ID", 26930530))
-API_HASH = os.environ.get("API_HASH", "b578cec1f4f5164d952c5a785a399a73")
+API_ID = int(os.environ.get("API_ID", 29426486))
+API_HASH = os.environ.get("API_HASH", "d71ad4ec048ab41677a1a439b21ff0c9")
 
 # Bot token
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6510599693:AAFxoVijOtGDcSDVmpU2XSwpRkqRqdXB9i8")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7355006985:AAEY8ijg4CP-8GgcliRGJja87Tby78QT7To")
 
 # Pyrogram session name
-SESSION_NAME = os.environ.get("SESSION_NAME")
+SESSION_NAME = os.environ.get("bot")
 
 # MongoDB URI (if you're using MongoDB)
 MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://aio:aio@aio.5z4gxok.mongodb.net/?retryWrites=true&w=majority")
